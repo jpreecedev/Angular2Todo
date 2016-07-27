ea213a7e4bda52e0ea65b4da5b0935417f956ced
Jon Preece <Jon Preece>
2016-07-27 06:31:09 +0100

    Added todomvc, wired up logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { TodoStore } from './app/services/store';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [TodoStore]);
